078321e23cbca43caccbd74bed7482f4e9af538b
Nameyray <eparis@atlassian.com>
2022-05-31 14:03:49 +0300

    create a method to check user credentials

--- app/src/main/java/com/moringaschool/sassygal/RegisterActivity.java
+++ app/src/main/java/com/moringaschool/sassygal/RegisterActivity.java
@@ -43,53 +43,53 @@ public class RegisterActivity extends AppCompatActivity {
 //        TextEmailAddress= findViewById(R.id.editTextTextEmailAddress);
 //        TextPassword= findViewById(R.id.editTextTextPassword);
 //        btnRegister=findViewById(R.id.button3);
-//        btnRegister.setOnClickListener(new View.OnClickListener(){
-//
-//            @Override
-//            public void onClick(View v) {
-//                checkCredentials();
-//
-//            }
-//        });
-//
-//        //onclickListener method for the button
-//        btnRegister.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(RegisterActivity.this, ProductLIstActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//    }
-//
-//    //method to check the register method credentials
-//    private void checkCredentials() {
-//        String textPersonName2= TextPersonName2.getText().toString();
-//        String EmailAddress = TextEmailAddress.getText().toString();
-//        String Password = TextPassword.getText().toString();
-//
-//        if (textPersonName2.isEmpty() || TextPersonName2.length()<3){
-//            showError(TextPersonName2, "Your name is not valid");
-//
-//        }else if (EmailAddress.isEmpty()  || !EmailAddress.contains("@")){
-//            showError(TextEmailAddress, "Email is not valid");
-//
-//        }else if(Password.isEmpty()  ||Password.length()<3){
-//            showError(TextPassword, "Password must be 3 characters");
-//        }else{
-//
-//            //create a toast popup for the register activity
-//            Toast.makeText(this, "Registration method", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    //method to show error
-//    private void showError(EditText input, String string) {
-//        input.setError(string);
-//        input.requestFocus();
-//
-//        }
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                checkCredentials();
+
+            }
+        });
+
+        //onclickListener method for the button
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, ProductLIstActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    //method to check the register method credentials
+    private void checkCredentials() {
+        String textPersonName2= TextPersonName2.getText().toString();
+        String EmailAddress = TextEmailAddress.getText().toString();
+        String Password = TextPassword.getText().toString();
+
+        if (textPersonName2.isEmpty() || TextPersonName2.length()<3){
+            showError(TextPersonName2, "Your name is not valid");
+
+        }else if (EmailAddress.isEmpty()  || !EmailAddress.contains("@")){
+            showError(TextEmailAddress, "Email is not valid");
+
+        }else if(Password.isEmpty()  ||Password.length()<3){
+            showError(TextPassword, "Password must be 3 characters");
+        }else{
+
+            //create a toast popup for the register activity
+            Toast.makeText(this, "Registration method", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //method to show error
+    private void showError(EditText input, String string) {
+        input.setError(string);
+        input.requestFocus();
+
+        }
 
     }
 
