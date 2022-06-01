f7234cfd6057f11bb2b7b9ceea7e52677bf784d1
Nameyray <eparis@atlassian.com>
2022-06-01 20:35:56 +0300

    update the register checkin credentials

--- app/src/main/java/com/moringaschool/sassygal/RegisterActivity.java
+++ app/src/main/java/com/moringaschool/sassygal/RegisterActivity.java
@@ -26,77 +26,68 @@ public class RegisterActivity extends AppCompatActivity {
     EditText textPassword;
     @BindView(R.id.button3)
     Button btnRegister;
-    private EditText editTextTextPersonName2, editTextTextEmailAddress, editTextTextPassword;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //binding the properties using butterknife
+        ButterKnife.bind(this);
 
-        //login if already registered
-        TextView log = findViewById(R.id.textView4);
-        editTextTextPersonName2= findViewById(R.id.editTextTextPersonName2);
-        editTextTextEmailAddress= findViewById(R.id.editTextTextEmailAddress);
-        editTextTextPassword = findViewById(R.id.editTextTextPassword);
-
-
-        log.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
             }
         });
-
-        //binding the properties using butterknife
-        ButterKnife.bind(this);
-
-        btnRegister.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                checkCredentials();
-
-            }
-        });
+      
 
         //onclickListener method for the button
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, ProductListActivity.class);
-                startActivity(intent);
+                if(checkCredentials()){
+                    Intent intent = new Intent(RegisterActivity.this, ProductListActivity.class);
+                    startActivity(intent);
+                }
+
             }
         });
 
     }
 
     //method to check the register method credentials
-    private void checkCredentials() {
+    private boolean checkCredentials() {
         String personName2 = textPersonName2.getText().toString();
         String emailAddress = textEmailAddress.getText().toString();
         String password = textPassword.getText().toString();
+        boolean isCorrect = false;
 
-        if (personName2.isEmpty() || textPersonName2.length()<3){
-            showError( "Your name is not valid");
+        if (personName2.isEmpty()  || !personName2.contains("@")){
+            showError("Name is not valid");
 
         }else if (emailAddress.isEmpty()  || !emailAddress.contains("@")){
-            showError( "Email is not valid");
+                showError("Email is not valid");
 
         }else if(password.isEmpty()  ||password.length()<3){
-            showError( "Password must be 3 characters");
+            showError("Password must be 3 characters");
         }else{
-
-            //create a toast popup for the register activity
-            Toast.makeText(this, "Registration method", Toast.LENGTH_SHORT).show();
+            //create a toast popup for the Register method
+            Toast.makeText(this, "Register method", Toast.LENGTH_SHORT).show();
+            isCorrect = true;
         }
+        return isCorrect;
     }
+
+
 
     //method to show error
     private void showError( String string) {
         Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
 
-        }
 
     }
+
+
+}
 
