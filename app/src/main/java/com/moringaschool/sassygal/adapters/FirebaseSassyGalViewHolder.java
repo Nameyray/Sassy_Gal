54fcc30ce8280469f0bb3f6929b20c83331a94e0
Nameyray <eparis@atlassian.com>
2022-06-16 15:49:48 +0300

    update

---
+++ app/src/main/java/com/moringaschool/sassygal/adapters/FirebaseSassyGalViewHolder.java
@@ -0,0 +1,17 @@
+package com.moringaschool.sassygal.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FirebaseSassyGalViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public FirebaseSassyGalViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
