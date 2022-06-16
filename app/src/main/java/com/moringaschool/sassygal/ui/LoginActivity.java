25484ceeea4a28e5fe0f244cd6e06512eec0b430
Nameyray <eparis@atlassian.com>
2022-06-16 15:34:08 +0300

    update

--- app/src/main/java/com/moringaschool/sassygal/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/sassygal/ui/LoginActivity.java
@@ -1,22 +1,35 @@
 
 package com.moringaschool.sassygal.ui;
 
+import static com.moringaschool.sassygal.ui.CreateAccountActivity.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.moringaschool.sassygal.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     //declaring the properties to be used in the login activity
     @BindView(R.id.textView7)
@@ -27,61 +40,110 @@ public class LoginActivity extends AppCompatActivity {
     EditText textPassword;
     @BindView(R.id.button4)
     Button btnLogin;
+    @BindView(R.id.firebaseProgressBar)
+    ProgressBar mSignInProgressBar;
+    @BindView(R.id.loadingTextView)
+    TextView mLoadingSignUp;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
-
-        //Register if not registered
-        TextView log = findViewById(R.id.textView7);
-        log.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
-
-
         ButterKnife.bind(this);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        //set the instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View v) {
-                if (checkCredentials()) {
-                    Intent intent = new Intent(LoginActivity.this, ProductSearchActivity.class);
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null ){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
+                    finish();
+                    
                 }
             }
+        };
 
-        });
-
+        btnLogin.setOnClickListener(this);
+        btn.setOnClickListener(this);
     }
-    //Method to check the credentials for logging in
 
-    private boolean checkCredentials () {
-        String emailAddress = textEmailAddress.getText().toString();
-        String password = textPassword.getText().toString();
-        boolean isCorrect = false;
-
-        //if else statement to ensure right credentials are entered
-        if (emailAddress.isEmpty() || !emailAddress.contains("@")) {
-            showError("Email is not valid");
-
-        } else if (password.isEmpty() || password.length() < 3) {
-            showError("Password must be 3 characters");
-        } else {
-            //create a toast popup for the login method
-            Toast.makeText(this, "Login method", Toast.LENGTH_SHORT).show();
-            isCorrect = true;
+    @Override
+    public void onClick(View view) {
+        if (view == btn) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
         }
-        return isCorrect;
+
+        if (view == btnLogin) {
+            loginWithPassword();
+            showProgressBar();
+
+        }
     }
 
-    //method to show error
-    private void showError (String string){
-        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+    //method to check email and password are not left blank
+    private void loginWithPassword() {
+        String email = textEmailAddress.getText().toString().trim();
+        String password = textPassword.getText().toString().trim();
+        if (email.equals("")) {
+            textEmailAddress.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            textPassword.setError("Password cannot be blank");
+            return;
+        }
+        //enable user to login
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
 
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                      hideProgressBar();
+
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    private void showProgressBar() {
+        mSignInProgressBar.setVisibility(View.VISIBLE);
+        mLoadingSignUp.setVisibility(View.VISIBLE);
+        mLoadingSignUp.setText("Log in you are in");
+    }
+
+    private void hideProgressBar() {
+        mSignInProgressBar.setVisibility(View.GONE);
+        mLoadingSignUp.setVisibility(View.GONE);
     }
 
 }
